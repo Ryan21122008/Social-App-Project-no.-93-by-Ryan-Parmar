827543274324b288da67744a773875f4b6e8cafd
Ryan21122008 <77188055+Ryan21122008@users.noreply.github.com>
2022-01-14 14:25:21 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDG3ff1wfs9gpkIjDKTzKfyc-vYHnnotho",
+    authDomain: "social-app-project-93-94-ryan.firebaseapp.com",
+    projectId: "social-app-project-93-94-ryan",
+    storageBucket: "social-app-project-93-94-ryan.appspot.com",
+    messagingSenderId: "918816531513",
+    appId: "1:918816531513:web:61b6400a3f0a80988ad9e8"
+  };
+  firebase.initializeApp(firebaseConfig);
